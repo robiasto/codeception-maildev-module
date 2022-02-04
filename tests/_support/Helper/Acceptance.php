4dc341e105c9b5427aac791a129c79ef00ac3464
Marc Bastian Heinrichs <typo3@mbh-software.de>
2022-02-04 15:50:39 +0100

    [TASK] Update phpmailer

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -1,6 +1,8 @@
 <?php
 namespace Helper;
 
+use PHPMailer\PHPMailer\PHPMailer;
+
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
@@ -8,15 +10,18 @@ class Acceptance extends \Codeception\Module
 {
     public function sendEmail($to, $subject, $body)
     {
-        $phpmailer = new \PHPMailer();
+        $phpmailer = new PHPMailer();
         $phpmailer->isSMTP();
         $phpmailer->Host = '127.0.0.1';
         $phpmailer->Port = 1025;
         $phpmailer->SMTPAutoTLS = false;
 
+        $phpmailer->setFrom("sender@example.com");
         $phpmailer->addAddress($to);
         $phpmailer->Subject = $subject;
         $phpmailer->Body = $body;
-        $phpmailer->send();
+        if (!$phpmailer->send()){
+            throw new \Exception($phpmailer->ErrorInfo);
+        };
     }
 }
